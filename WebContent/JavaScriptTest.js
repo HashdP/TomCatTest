eec6473b4a90e100ce13668802d8a23ff5f6b0ac
Yorozu <kodfji06@gmail.com>
2019-05-30 00:24:18 +0900

    アニメーション実装

--- WebContent/JavaScriptTest.js
+++ WebContent/JavaScriptTest.js
@@ -8,7 +8,7 @@ var headerSize = 200;
 var menuSize = 200;
 
 /*あ中心にボタンを描画する */
-function drawButton(buttonCanvas, color, scale, text = 'テキスト未定義ですですよ～ん'){
+function drawButton(buttonCanvas, color, scale, text = 'テキスト未定義'){
 	var context = buttonCanvas.getContext('2d');
 	
 	context.clearRect(0,0,buttonCanvas.width,buttonCanvas.height);
@@ -30,7 +30,6 @@ function drawButton(buttonCanvas, color, scale, text = 'テキスト未定義で
 	context.font = `bold ${20 * scale}px "MS ゴシック"`;
 	context.textAlign = "center";
 	context.textBaseline = "middle";
-	context.measureText(text);
 	context.fillText(text, cCenterX, cCenterY);
 }
 
@@ -44,10 +43,10 @@ function draw(){
 		context.fillRect(0,0,sw,headerSize);
 		
 		context.fillStyle = '#FFFFFF';
-		context.font = "bold 200px 'MS ゴシック'";
+		context.font = "bold 120px 'MS ゴシック'";
 		context.textAlign = "left";
 		context.textBaseline = "top";
-		context.fillText("テキストん", 0, 0);
+		context.fillText("テキスト", 50, headerSize-120);
 	}
 	
 	if(menuCanvas.getContext){
@@ -95,18 +94,10 @@ function reload(){
 	draw();
 }
 
-function onMouseDown1(){
-	drawButton(testButton1Canvas, "#800000", 1.0);
-}
-function onMouseUp1(){
-	drawButton(testButton1Canvas, "#800000", 0.95);
-}
-function onMouseOver1(){
-	drawButton(testButton1Canvas, "#800000", 0.95);
-}
-function onMouseOut1(){
-	drawButton(testButton1Canvas, "#696969", 0.95);
-}
+function onMouseDown1(){ drawButton(testButton1Canvas, "#800000", 1.0);  }
+function onMouseUp1(){   drawButton(testButton1Canvas, "#800000", 0.95); }
+function onMouseOver1(){ drawButton(testButton1Canvas, "#800000", 0.95); flipSquare(0.2, "#00ff00"); }
+function onMouseOut1(){  drawButton(testButton1Canvas, "#696969", 0.95); flipSquare(0.2, "#ff0000"); }
 
 window.addEventListener('resize', reload, false);
 testButton1Canvas.addEventListener('mousedown', onMouseDown1, false);
@@ -115,3 +106,115 @@ testButton1Canvas.addEventListener('mouseover', onMouseOver1, false);
 testButton1Canvas.addEventListener('mouseout', onMouseOut1, false);
 resize();
 
+/*comment canvasのアニメーション*/
+const timeCanvas = document.getElementById('time');
+
+function drawTime(ctx, t) {
+	ctx.save();
+    
+    /*comment　イージング*/
+    var easeInOutCubic = function (t, b, c, d) {
+    	t /= d/2;
+    	if (t < 1) return c/2*t*t*t + b;
+    	t -= 2;
+    	return c/2*(t*t*t + 2) + b;
+    };
+	
+	/*comment 描画をクリア*/
+	ctx.clearRect(0,0,timeCanvas.width,timeCanvas.height);
+	
+	/*comment 時計を描画*/
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+    ctx.font = devicePixelRatio * 20 + 'px Courier,monospace';
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(new Date().toISOString(), timeCanvas.width / 2, timeCanvas.height / 2);
+    ctx.fillText((1000 / (t - draw.lastT)).toFixed(3) + 'FPS', timeCanvas.width / 2, timeCanvas.height / 2 + devicePixelRatio * 20);
+    draw.lastT = t;
+    
+    /*comment 円を描画*/
+    const phase = (t % 2000) / 2000;
+    const phase1 = easeInOutCubic(phase, 0, 1, 1);
+    const phase2 = easeInOutCubic(2 * Math.max(phase - 0.5, 0), 0, 1, 1);
+    const radius = 0.4 * Math.min(timeCanvas.width, timeCanvas.height);
+    const PI2 = Math.PI * 2;
+    ctx.beginPath();
+    ctx.translate(timeCanvas.width/2, timeCanvas.height/2);
+    ctx.rotate(-0.5 * Math.PI);
+    ctx.arc(0, 0, radius, PI2 * phase2, PI2 * phase1);
+    ctx.strokeStyle='#000000';
+    ctx.lineWidth=radius/6;
+    ctx.lineCap = 'round';
+    ctx.stroke();
+    
+    ctx.restore();
+}
+
+requestAnimationFrame(function (t0) {
+    const ctx = timeCanvas.getContext('2d');
+    render(t0);
+    function render(t1) {
+    	drawTime(ctx, t1 - t0);
+    	requestAnimationFrame(render);
+    	
+    	/*
+    	const progress = (t1-t0) / 1000;
+    	
+    	if(progress < 3){
+            requestAnimationFrame(render);
+    	}
+    	else{
+    	}
+    	*/
+    }
+});
+
+
+/*comment フリップアニメーション*/
+const flipCanvas = document.getElementById('flip');
+var flipCanvasColor = "#ff0000"
+
+function drawFlipSquare(theta){
+	var ctx = flipCanvas.getContext('2d');
+	
+	ctx.clearRect(0,0,flip.width,flip.height);
+	
+	ctx.fillStyle = "#000000";
+	ctx.fillRect(0,0,flip.width,flip.height);
+	
+	var radius = Math.sqrt(Math.pow(flip.width/2, 2) + Math.pow(flip.height/2, 2));
+	var diagonalLength = radius * Math.cos(theta);
+	var diagonalPos = diagonalLength / Math.sqrt(2);
+	
+	ctx.fillStyle = flipCanvasColor;
+	ctx.beginPath();
+	ctx.moveTo(flip.width/2 - diagonalPos, flip.height/2 - diagonalPos);
+	ctx.lineTo(200,   0);
+	ctx.lineTo(flip.width/2 + diagonalPos, flip.height/2 + diagonalPos);
+	ctx.lineTo(  0, 200);
+	ctx.closePath();
+	ctx.fill();
+}
+
+function flipSquare(time, toColor){
+	requestAnimationFrame(function (t0) {
+	    render(t0);
+	    function render(t1) {
+	    	const progress = (t1-t0) / 1000;
+	    	var theta = Math.PI * (progress / time);
+	    	
+	    	if(progress < time/2){
+		    	drawFlipSquare(theta);
+	    	    requestAnimationFrame(render);
+	    	}else if(progress < time){
+	    		flipCanvasColor = toColor;
+		    	drawFlipSquare(theta);
+	    	    requestAnimationFrame(render);
+	    	}else{
+	    		drawFlipSquare(Math.PI);
+	    	}
+	    }
+	});
+}
+
+drawFlipSquare(Math.PI);
--- WebContent/WEB-INF/ShowDate.jsp
+++ WebContent/WEB-INF/ShowDate.jsp
@@ -14,44 +14,49 @@
 	<div class = "canvas-wrapper">
 		<canvas id="testButton1" width="210" height="63"></canvas>
 		<canvas id="testButton2" width="200" height="60"></canvas>
+		<canvas id="time"></canvas>
 		<canvas id="header"></canvas>
 		<canvas id="menu"></canvas>
+		
+		<canvas id="flip", width="200" height="200"></canvas>
+		
+		<pre id="sorceCode"><code>
+package pack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ShowDateServlet
+ */
+@WebServlet(name = "ShowDate", urlPatterns = {"/ShowDate"})
+public class ShowDateServlet extends HttpServlet 
+    implements javax.servlet.Servlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	    throws ServletException, IOException
+	{
+		request.setAttribute("test", "Javaから渡された値です");
+		
+		String view = "/WEB-INF/ShowDate.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(view);
+		
+		dispatcher.forward(request, response);
+	}
+}
+		</code></pre>>
+		
 	</div>
 	
 	<script type="text/javascript" src="./JavaScriptTest.js"></script>
-    
-    <!-- 
-    <form action="http://localhost:8080/WebHello/DisplayRequest" method="post">
-		<table border="1">
-			<tr>
-				<th>姓</th>
-				<td><input type="text" name="sei"></td>
-				<th>名</th>
-				<td><input type="text" name="mei"></td>
-			</tr>
-			<tr>
-				<th>性別</th>
-				<td>男性：<input type="radio" name="sex" value="男性">女性：<input
-					type="radio" name="sex" value="女性"></td>
-			</tr>
-			<tr>
-				<th>年齢１</th>
-				<td><select name="age">
-						<option value="10歳未満">10歳未満
-						<option value="10代">10代
-						<option value="20代">20代
-						<option value="30代">30代
-						<option value="40代">40代
-						<option value="50代">50代
-						<option value="60代以上">60代以上
-				</select></td>
-			</tr>
-			<tr>
-				<td><input type="submit" value="次へ" name="test"></td>
-			</tr>
-		</table>
-	</form>
-	 -->
 	 
 </body>
 
