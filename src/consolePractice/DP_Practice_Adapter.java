e9f279d34ebe3c3d095566dad02349cb8fc3d29a
Yorozu <kodfji06@gmail.com>
2019-05-31 15:44:17 +0900

    デザインパターンの練習２

--- src/consolePractice/DP_Practice_Adapter.java
+++ src/consolePractice/DP_Practice_Adapter.java
@@ -1,5 +1,34 @@
 package consolePractice;
 
-public class DP_Practice_Adapter {
+class Taro{
+	public void enjoyWithAllClassmate() {
+		System.out.println("みんなで楽しむ");
+	}
+}
 
+interface Chairperson{
+	public void organizeClass();
+}
+
+class NewTaro extends Taro implements Chairperson{
+	public void organizeClass() {
+		enjoyWithAllClassmate();
+	}
+}
+
+class Hanako implements Chairperson{
+	private Taro taro;
+	public Hanako() {
+		taro = new Taro();
+	}
+	public void organizeClass() {
+		taro.enjoyWithAllClassmate();
+	}
+}
+
+public class DP_Practice_Adapter {
+	public static void main(String args[]) {
+		Chairperson chairperson = new Hanako();
+		chairperson.organizeClass();
+	}
 }
